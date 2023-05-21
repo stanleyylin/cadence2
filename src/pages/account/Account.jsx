b1cda470b67901f9a41f6908c813dd94eff11688
stanleyylin <stanleylin82@gmail.com>
2023-05-21 07:30:02 -0400

    account

---
+++ src/pages/account/Account.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Song from '../../components/Song';
+import carl from '../../res/carl.png';
+import './feed.css';
+
+const Account = () => {
+  return (
+    <div className='feed-container'>
+      
+      
+    </div>
+  )
+}
+
+export default Account
