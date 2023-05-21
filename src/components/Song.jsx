a5b6883c74b09e74799e91fce29159608a675bd0
stanleyylin <stanleylin82@gmail.com>
2023-05-21 03:47:39 -0400

    songs

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+import './song.css';
 
 const Song = ({song}) => {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    const importImage = async () => {
+      const importedImage = await import(`../res/songs/${song.imageSrc}`);
+      setImage(importedImage.default);
+    };
+
+    importImage();
+  }, [song.imageSrc]);
 
   return (
+
+    
     <div className='song-container'>
-      <img src={song.imageSrc}/>
-      <h2>{song.name}</h2>
-      <p>{song.artist}</p>
-      <p>{song.year}</p>
-      <p>{song.cost}</p>
-      <button></button>
+      <img src={image}/>
+      <div className='lower'>
+        <h2>{song.name}</h2>
+        <p>{song.artist}</p>
+        <p>{song.year}</p>
+        <p>{song.cost}</p>
+        <button className='buy'>BUY</button>
+      </div>
+      
       
     </div>
   )
 }
+
 
 export default Song
--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -6,18 +6,74 @@ import './feed.css';
 const Feed = () => {
   const songs = [
     {
-      imageSrc: "../res/artist1.png",
-      name: "The Weekend",
+      imageSrc: "song1.png",
+      name: "Snooze",
       artist: "SZA",
       year: "2022",
-      price: 2,
+      cost: "2 Eth",
+    },
+    {
+      imageSrc: "song2.png",
+      name: "Jackie Brown",
+      artist: "Brent Faiyaz",
+      year: "2022",
+      cost: "1 Eth",
+    },
+    {
+      imageSrc: "song3.png",
+      name: "Moonlight",
+      artist: "Kali Uchis",
+      year: "2023",
+      cost: "1 Eth",
+    },
+    {
+      imageSrc: "song4.png",
+      name: "Deserve Me",
+      artist: "Kali Uchis",
+      year: "2023",
+      cost: "2 Eth",
+    },
+    {
+      imageSrc: "song5.png",
+      name: "America Has a Problem",
+      artist: "Beyonce ft. Kendrick",
+      year: "2023",
+      cost: "2 Eth",
+    },
+    {
+      imageSrc: "song6.png",
+      name: "Pink + White",
+      artist: "Frank Ocean",
+      year: "2016",
+      cost: "2 Eth",
+    },
+    {
+      imageSrc: "song7.png",
+      name: "Slide",
+      artist: "Calvin Harris",
+      year: "2017",
+      cost: "3 Eth",
+    },
+    {
+      imageSrc: "song8.png",
+      name: "The Weekend",
+      artist: "SZA",
+      year: "2017",
+      cost: "3 Eth",
+    },
+    {
+      imageSrc: "song9.png",
+      name: "Drew Barrymore",
+      artist: "SZA",
+      year: "2017",
+      cost: "3 Eth",
     },
   ]
   return (
     <div className='feed-container'>
       <img src={carl} className='feed-bg'/>
-      <p>EXPLORE</p>
-      <div>
+      <p className='title-explore'>YOUR FEED</p>
+      <div className='song-grid'>
         {songs.map((currSong) => (
           <Song 
             song = {currSong}
