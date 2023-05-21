9a8c3861118f0699a6c31707f89d4933803a0fd6
Kelly Qu <116982102+kellyquu@users.noreply.github.com>
2023-05-21 02:59:44 -0400

    upload page

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import carl from '../res/carl.png';
+import './UploadPage.css';
 import UploadButton from '../components/UploadButton';
 
 const UploadPage = () => {
   return (
-    <div>
-      <h1>Upload Page</h1>
-      <UploadButton />
+    <div className='feed-container'>
+      <img src={carl} className='feed-bg'/>
+      <div className='heading-subcontainer'>
+        <h1>Upload Page</h1>
+        <p>Upload your music here.</p> 
+            <div>
+            <h1>Upload Page</h1>
+            <UploadButton />
+            </div>
+      </div>
     </div>
-  );
-};
+  )
+}
 
 export default UploadPage;
