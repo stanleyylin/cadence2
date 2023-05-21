da8b6d6330738951d6cb1d8d8cb7ec4428231314
Kelly Qu <116982102+kellyquu@users.noreply.github.com>
2023-05-21 08:09:10 -0400

    typing effect

--- src/pages/landing/Header.jsx
+++ src/pages/landing/Header.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './header.css';
 import video from '../../res/header-bg.mp4';
 
 const Header = () => {
+  const [typingText, setTypingText] = useState('');
+
+  useEffect(() => {
+    const textToType = '1, 2, 3.';
+    let currentIndex = 0;
+
+    const typingInterval = setInterval(() => {
+      setTypingText((prevText) => {
+        const shouldDelete = prevText.length > 0 && currentIndex === textToType.length;
+        const nextIndex = shouldDelete ? currentIndex - 1 : currentIndex;
+
+        return shouldDelete
+          ? prevText.slice(0, nextIndex)
+          : textToType.slice(0, nextIndex + 1);
+      });
+
+      currentIndex = (currentIndex + 1) % (textToType.length + 1);
+    }, 200);
+
+    return () => clearInterval(typingInterval);
+  }, []);
+
   return (
     <header className='heading-container'>
       <div>
         <video autoPlay loop muted className='video-element'>
-        <source src={video} type='video/mp4' />
+          <source src={video} type='video/mp4' />
         </video>
         <div className='video-overlay'></div>
       </div>
       <div className='heading-subcontainer'>
         <h1>MEET CADENCE</h1>
-        <p>Music for all your video PROJECT NEEDS. Powered by blockchain, SEAMLESS TO USE, and as easy as 1, 2, 3.</p> 
+        <p>
+          Music for all your video PROJECT NEEDS. Powered by blockchain, SEAMLESS TO USE, and as
+          easy as {typingText}
+        </p>
       </div>
-
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
